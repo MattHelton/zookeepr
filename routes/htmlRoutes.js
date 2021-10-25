ac06c879d8331509ef1c23ccd69bb4ffd28b51f4
matthelton <mjoelhelton@me.com>
2021-10-25 14:10:28 -0400

    Lesson 11.3.6 Create Routes to Serve Other Pages

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,6 +5,13 @@ const path = require('path');
 
 const router = express.Router()
 
+
+router.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/animals.html'));
+})
+router.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/zookeepers.html'));
+})
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
