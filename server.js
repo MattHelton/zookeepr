08cf1648dc818867dcd1975b63fc49d015111d49
matthelton <mjoelhelton@me.com>
2021-10-25 10:39:21 -0400

    Lesson 11.1.4 Create and Express Server

---
+++ server.js
@@ -0,0 +1,9 @@
+// imports express package
+const express = require("express");
+// instantiates the server
+const app = express();
+
+// makes the server listen
+app.listen(3001, () =>{
+  console.log("API server now running on port 300!1");
+});
